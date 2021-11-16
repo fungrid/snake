2232b29e34d7ae0af1fc3cdb08cb4193318d3c85
Peter Måseide <peter@agens.no>
2021-11-16 16:42:18 +0100

    Oppdater main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+def sneka(x: number, y: number):
+    indeks = 0
+    while indeks <= len(X):
+        if X[indeks] == x and Y[indeks] == y:
+            return 1
+        indeks += 1
+    return 0
+
 def on_button_pressed_a():
-    global R, B
-    if B == 0:
-        R = R - 1
-    B = 1
+    global State, R, B
+    if State == 0:
+        State = 1
+        led.stop_animation()
+        led.plot_brightness(HX, HY, 128)
+        led.plot_brightness(EX, EY, 255)
+    else:
+        if B == 0:
+            R = R - 1
+        B = 1
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
@@ -12,42 +26,59 @@ def on_button_pressed_b():
     B = 1
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+EY = 0
+State = 0
+EX = 0
 B = 0
 R = 0
+Y: List[number] = []
+X: List[number] = []
+HY = 0
+HX = 0
 HX = -1
-HY = 2
+HY = 1
 X = [-1]
 Y = [2]
 R = 1
 B = 0
-led.plot_brightness(HX, HY, 128)
+EX = randint(1, 4)
+State = 0
+EY = randint(3, 4)
 
 def on_every_interval():
-    global R, HX, HY, B
-    led.unplot(HX, HY)
-    if R < 0:
-        R = 3
-    R = R % 4
-    if R == 1:
-        HX = HX + 1
-    elif R == 2:
-        HY = HY + 1
-    elif R == 3:
-        HX = HX - 1
-    else:
-        HY = HY - 1
-    if HX < 0:
-        HX = 4
-    HX = HX % 5
-    if HY < 0:
-        HY = 4
-    HY = HY % 5
-    X.append(HX)
-    Y.append(HY)
-    led.plot_brightness(HX, HY, 128)
-    B = 0
+    global R, HX, HY, EX, EY, B
+    if State == 1:
+        if R < 0:
+            R = 3
+        R = R % 4
+        if R == 1:
+            HX = HX + 1
+        elif R == 2:
+            HY = HY + 1
+        elif R == 3:
+            HX = HX - 1
+        else:
+            HY = HY - 1
+        if HX < 0:
+            HX = 4
+        HX = HX % 5
+        if HY < 0:
+            HY = 4
+        X.append(HX)
+        Y.append(HY)
+        if EY == HY and EX == HX:
+            while EY == HY and EX == HX:
+                EX = randint(0, 4)
+                EY = randint(0, 4)
+                led.plot_brightness(EX, EY, 255)
+        else:
+            led.unplot(X.shift(), Y.shift())
+        led.plot_brightness(HX, HY, 128)
+        B = 0
+        HY = HY % 5
 loops.every_interval(1000, on_every_interval)
 
 def on_forever():
-    pass
+    if State == 0:
+        basic.show_string("Sneka")
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
-input.onButtonPressed(Button.A, function () {
-    if (B == 0) {
-        R = R - 1
+function sneka (x: number, y: number) {
+    while (indeks <= X.length) {
+        if (X[indeks] == x && Y[indeks] == y) {
+            return 1
+        }
+        indeks += 1
     }
-    B = 1
+    return 0
+}
+input.onButtonPressed(Button.A, function () {
+    if (State == 0) {
+        State = 1
+        led.stopAnimation()
+        led.plotBrightness(HX, HY, 128)
+        led.plotBrightness(EX, EY, 255)
+    } else {
+        if (B == 0) {
+            R = R - 1
+        }
+        B = 1
+    }
 })
 input.onButtonPressed(Button.B, function () {
     if (B == 0) {
@@ -10,44 +26,65 @@ input.onButtonPressed(Button.B, function () {
     }
     B = 1
 })
+let indeks = 0
+let EY = 0
+let State = 0
+let EX = 0
 let B = 0
 let R = 0
-let HX = -1
-let HY = 1
-let X = [-1]
-let Y = [2]
+let Y: number[] = []
+let X: number[] = []
+let HY = 0
+let HX = 0
+HX = -1
+HY = 1
+X = [-1]
+Y = [2]
 R = 1
 B = 0
-led.plotBrightness(HX, HY, 128)
-led.plotBrightness(3, 3, 255)
+EX = randint(1, 4)
+State = 0
+EY = randint(3, 4)
 loops.everyInterval(1000, function () {
-    led.unplot(HX, HY)
-    if (R < 0) {
-        R = 3
+    if (State == 1) {
+        if (R < 0) {
+            R = 3
+        }
+        R = R % 4
+        if (R == 1) {
+            HX = HX + 1
+        } else if (R == 2) {
+            HY = HY + 1
+        } else if (R == 3) {
+            HX = HX - 1
+        } else {
+            HY = HY - 1
+        }
+        if (HX < 0) {
+            HX = 4
+        }
+        HX = HX % 5
+        if (HY < 0) {
+            HY = 4
+        }
+        X.push(HX)
+        Y.push(HY)
+        if (EY == HY && EX == HX) {
+            while (EY == HY && EX == HX) {
+                EX = randint(0, 4)
+                EY = randint(0, 4)
+                led.plotBrightness(EX, EY, 255)
+            }
+        } else {
+            led.unplot(X.shift(), Y.shift())
+        }
+        led.plotBrightness(HX, HY, 128)
+        B = 0
+        HY = HY % 5
     }
-    R = R % 4
-    if (R == 1) {
-        HX = HX + 1
-    } else if (R == 2) {
-        HY = HY + 1
-    } else if (R == 3) {
-        HX = HX - 1
-    } else {
-        HY = HY - 1
-    }
-    if (HX < 0) {
-        HX = 4
-    }
-    HX = HX % 5
-    if (HY < 0) {
-        HY = 4
-    }
-    HY = HY % 5
-    X.push(HX)
-    Y.push(HY)
-    led.plotBrightness(HX, HY, 128)
-    B = 0
 })
 basic.forever(function () {
-	
+    if (State == 0) {
+        basic.showString("Sneka")
+    }
 })
